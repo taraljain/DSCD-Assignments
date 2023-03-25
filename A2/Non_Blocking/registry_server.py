6eb322c7407e1d1e458eff0befe7f86c539b21b8
Shanu <63155782+Shanu85@users.noreply.github.com>
2023-03-25 12:16:15 +0530

    non-blocking code
    non-blocking code

--- A2/Non_Blocking/registry_server.py
+++ A2/Non_Blocking/registry_server.py
@@ -1,43 +1,40 @@
-import A2_pb2 
-import A2_pb2_grpc
 import grpc
 from concurrent import futures
 from urllib.parse import urlparse
-MAXSERVERS = 5
-servers=set()
-primaryServer=None
+import sys
 
+
+import A2_pb2
+import A2_pb2_grpc
+
+MAXSERVERS = 5
+SERVERS = set()
+primaryServer = None
 
 class RegistryServerServicer(A2_pb2_grpc.RegistryServerServicer):
     def RegisterServer(self, request, context):
-        request_ip=request.ip
-        request_port=request.port
-
-        print(f"JOIN REQUEST FROM {request_ip}:{request_port}")
-
-        # if max server limit is reached
-        if len(servers)==MAXSERVERS:
+        print(f'JOIN REQUEST FROM {request.ip}:{request.port}')
+        
+        if len(SERVERS) == MAXSERVERS:
             print("MAX CONNECTION LIMIT REACHED, REJECTING THE REQUEST FOR REGISTRATION")
             return A2_pb2.ServerDetails(ip="", port="")
-
-        new_server=(request_ip,request_port)
-        servers.add(new_server)
         
-        global primaryServer
-        
-        # if primaryServer is none, declare current server as primary server
-        if primaryServer==None:
-            primaryServer=new_server
-        
+        else:
+            server = (request.ip, request.port)
+            SERVERS.add(server)
+            
+            global primaryServer
+            if primaryServer == None:
+                primaryServer = server
 
-        with grpc.insecure_channel(f"{primaryServer[0]}:{primaryServer[1]}") as channel:
-            server_stub=A2_pb2_grpc.ServerStub(channel)
-            request=A2_pb2.ServerDetails(ip=request_ip,port=request_port)
-            server_stub.SendJoiningInfo(request)
-
-        # return the primary server details
-        return A2_pb2.ServerDetails(ip = primaryServer[0], port = primaryServer[1])
-
+            # Send joining info to the primary server
+            with grpc.insecure_channel(f'{primaryServer[0]}:{primaryServer[1]}') as channel:
+                stub = A2_pb2_grpc.ServerStub(channel)
+                request = A2_pb2.ServerDetails(ip=server[0], port=server[1])
+                stub.SendJoiningInfo(request)
+            
+            # Return the primary server's details
+            return A2_pb2.ServerDetails(ip = primaryServer[0], port = primaryServer[1])
 
     def GetServerList(self, request, context):
         client_address = urlparse(context.peer()).path
@@ -45,17 +42,15 @@ class RegistryServerServicer(A2_pb2_grpc.RegistryServerServicer):
         ip = client_address[:idx]
         port = client_address[idx+ 1:]
         print(f'SERVER LIST REQUEST FROM {ip}:{port}')
+        return A2_pb2.ServerList(servers=[A2_pb2.ServerDetails(ip=ip, port=port) for ip, port in SERVERS])
 
-        return A2_pb2.ServerList(servers=[A2_pb2.ServerDetails(ip=ip, port=port) for ip, port in servers])
-    
 
-def start(host,port):
-    registryServer=grpc.server(futures.ThreadPoolExecutor(max_workers=MAXSERVERS))
-    A2_pb2_grpc.add_RegistryServerServicer_to_server(RegistryServerServicer(),registryServer)
+def serve(host, port):
+    registryServer = grpc.server(futures.ThreadPoolExecutor(max_workers=MAXSERVERS))
+    A2_pb2_grpc.add_RegistryServerServicer_to_server(RegistryServerServicer(), registryServer)
     registryServer.add_insecure_port(f"{host}:{port}")
-    registryServer.start()
+    registryServer.start();
     registryServer.wait_for_termination()
 
-
-if __name__=='__main__':
-    start(host='localhost',port='6000')
+if __name__ == "__main__":
+    serve(host="localhost", port=6000)
--- A2/Non_Blocking/server.py
+++ A2/Non_Blocking/server.py
@@ -1,9 +1,13 @@
 import A2_pb2 
-import os
 import A2_pb2_grpc
 import grpc
 from concurrent import futures
+import sys
 import random
+import datetime
+import queue
+import threading
+import os
 MAXCLIENTS = 10
 DataStore = {}
 SERVERS = set()
@@ -45,7 +49,7 @@ class ServerServicer(A2_pb2_grpc.ServerServicer):
         key=request.UUID
         fileName=f"{port}/{request.name}.txt"
 
-        if send_to_primary_server(key,fileName):
+        if check_file_exists(key,fileName):
             with grpc.insecure_channel(f'{primaryServer[0]}:{primaryServer[1]}') as channel:
                 stub = A2_pb2_grpc.ServerStub(channel)
                 response=stub.WritePrimary(request)
@@ -56,15 +60,51 @@ class ServerServicer(A2_pb2_grpc.ServerServicer):
 
         elif key in DataStore and not os.path.isfile(fileName):
             return A2_pb2.WriteResponse(status="DELETED FILE CANNOT BE UPDATED", UUID="" ,version="")
-        
+    
 
     def WritePrimary(self, request, context):
         print("Write request received by primary server")
+        key=request.UUID
+        fileName=f"{port}/{request.name}.txt"
 
-    
+        if check_file_exists(key,fileName):
+            request_uuid=request.UUID
+            request_name=request.name
+            request_content=request.content
+
+            response_val=[None]*1
+
+            t1=threading.Thread(target=WritePrimaryHelper,args=(request_uuid,request_name,request_content,response_val))
+            t2=threading.Thread(target=WriteBackupRelicas,args=(request_uuid,request_name,request_content))
+            
+            t1.start()
+            t2.start()
+            
+            return response_val[0]
+
+        elif request.UUID not in DataStore and os.path.isfile(f'{port}/{request.name}.txt'):
+            return A2_pb2.WriteResponse(status="FILE WITH THE SAME NAME ALREADY EXISTS", UUID="" ,version="")
+        elif request.UUID not in DataStore and not os.path.isfile(f'{port}/{request.name}.txt'):
+            return A2_pb2.WriteResponse(status="DELETED FILE CANNOT BE UPDATED", UUID="" ,version="")
+
 
     def WriteServer(self, request, context):
-        return super().WriteServer(request, context)
+        print("Write request received by replica server")
+        key=request.UUID
+        fileName=f"{port}/{request.name}.txt"
+
+        if check_file_exists(key,fileName):
+            DataStore.update({request.UUID: (request.name, request.version)})
+
+            # Save request.content to the file system
+            saveFile(port, f'{request.name}.txt', request.content)
+
+            return A2_pb2.Response(status="SUCCESS")
+        
+        elif request.UUID not in DataStore and os.path.isfile(f'{port}/{request.name}.txt'):
+            return A2_pb2.WriteResponse(status="FILE WITH THE SAME NAME ALREADY EXISTS", UUID="" ,version="")
+        elif request.UUID not in DataStore and not os.path.isfile(f'{port}/{request.name}.txt'):
+            return A2_pb2.WriteResponse(status="DELETED FILE CANNOT BE UPDATED", UUID="" ,version="")
 
     def Delete(self, request, context):
         return super().Delete(request, context)
@@ -77,6 +117,48 @@ class ServerServicer(A2_pb2_grpc.ServerServicer):
     def DeleteServer(self, request, context):
         return super().DeleteServer(request, context)
 
+
+# write to primary replica only
+def WritePrimaryHelper(request_UUID,request_name,request_content,response_val):
+        current_time = str(datetime.datetime.now())
+        DataStore.update({request_UUID:{request_name,current_time}})
+
+        saveFile(port,f'{request_name}.txt',request_content)
+
+        response_val[0]=A2_pb2.WriteResponse(status="SUCCESS", UUID=request_UUID ,version=current_time)
+
+    # send write request to backup replicas
+def WriteBackupRelicas(request_UUID,request_name,request_content):
+        current_time = str(datetime.datetime.now())
+        # Create a queue to communicate with replica threads
+        q = queue.Queue()
+
+        def sendWriteRequestToServers(server):
+            with grpc.insecure_channel(f'{server[0]}:{server[1]}') as channel:
+                stub = A2_pb2_grpc.ServerStub(channel)
+                req = A2_pb2.WriteRequestServer(name=request_name, content=request_content, UUID=request_UUID, version=current_time)
+                response = stub.WriteServer(req)
+                    
+                q.put(response)
+
+
+        threads = [threading.Thread(target=sendWriteRequestToServers, args=(server,)) for server in SERVERS]
+        for thread in threads:
+            thread.start()
+
+        # Wait for all threads to finish and collect acknowledgements
+        responses = []
+        for thread in threads:
+            thread.join()
+            responses.append(q.get())
+        
+        # Check if all replicas successfully wrote the data
+        success = all(response.status=="SUCCESS" for response in responses)
+
+        if success:
+            print("Write request to all backup replicas completed successfully ...")
+        else:
+            print("Write request to some backup replicas failed ...")
 
 def start(host,port):
     server=grpc.server(futures.ThreadPoolExecutor(max_workers=MAXCLIENTS))
@@ -92,7 +174,7 @@ def saveFile(folder, fileName, content):
     with open(os.path.join(folder, fileName), 'w') as file:
         file.write(content)
 
-def send_to_primary_server(key,fileName):
+def check_file_exists(key,fileName):
     if key not in DataStore and not os.path.isfile(fileName):
         return True
     
