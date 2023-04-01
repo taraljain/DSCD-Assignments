811d5af86ac8e06f2bf987fd221ef959085eeac9
Taral Jain <taral19392@iiitd.ac.in>
2023-04-01 12:06:22 +0530

    Removed Unnecessary Imports

--- A2/Blocking/TestScript.py
+++ A2/Blocking/TestScript.py
@@ -1,21 +1,13 @@
-import threading
 import subprocess
-import multiprocessing
-import RegistryServer
-import Server
 import Client
 import sys
 sys.path.insert(1, "../")
-import A2_pb2
-import A2_pb2_grpc
 import time
 
 
 def simulate():
     IP = "localhost"
     registryServerPort = "6000"
-
-    subprocesses = []
     
     # create registry server
     subprocess.Popen(['gnome-terminal', '--', 'python', 'RegistryServer.py', IP, registryServerPort])
